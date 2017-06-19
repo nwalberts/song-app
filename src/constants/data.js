c7b6b06ddf0082cbdfcca5f28b4a8a81c258e4c9
Nick Alberts <nwalberts5@gmail.com>
2017-06-19 13:48:40 -0400

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,5 @@
+const data = [
+  { title: "Ben Rector - Note to Self", playing: false, duration: "4:30" },
+  { title: "Dan Mangan - Basket", playing: false, duration: "3:40" },
+  { title: "John Mayer - Gravity", playing: true, duration: "6:04" }
+];
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Playlist from './components/Playlist.js';
+
+const data = [
+  { id: 1, title: "Ben Rector - Note to Self", playing: false, duration: "3:02" },
+  { id: 2, title: "Dan Mangan - Basket", playing: false, duration: "3:52" },
+  { id: 3, title: "John Mayer - Gravity", playing: true, duration: "4:06" }
+];
+
+
+
+ReactDOM.render(
+  <Playlist data={data} />,
+  document.getElementById('app')
+);
